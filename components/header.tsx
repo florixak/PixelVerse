152de7d3b024e7ac4f4a65ed4813e52de33438d1
ptakondrej <florixakcz@seznam.cz>
2025-05-28 12:00:07 +0200

    Edit auth buttons

--- components/header.tsx
+++ components/header.tsx
@@ -6,13 +6,22 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import { ThemeSwitcher } from "./theme-switcher";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <header className="flex justify-end items-center p-4 gap-4 h-16">
       <SignedOut>
-        <SignInButton mode="modal" />
-        <SignUpButton mode="modal" />
+        <SignInButton mode="modal">
+          <Button variant="outline" size="sm">
+            Sign In
+          </Button>
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button variant="default" size="sm">
+            Sign Up
+          </Button>
+        </SignUpButton>
       </SignedOut>
       <ThemeSwitcher />
     </header>
