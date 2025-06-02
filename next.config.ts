d55064b034c991940ec7f088d206b995afaebe3d
ptakondrej <ptakondrej@spseplzen.cz>
2025-06-02 22:27:31 +0200

    Add remote pattern for clerk images

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/images/**",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
