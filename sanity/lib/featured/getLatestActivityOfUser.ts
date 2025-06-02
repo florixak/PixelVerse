67a592373bfb0be19bf3521178bd483b5dd8038a
ptakondrej <florixakcz@seznam.cz>
2025-06-02 14:21:29 +0200

    Create get user latest activities

---
+++ sanity/lib/featured/getLatestActivityOfUser.ts
@@ -0,0 +1,29 @@
+import { groq } from "next-sanity";
+import { client } from "../client";
+import { Post } from "@/sanity.types";
+
+const getLatestActivityOfUser = async (
+  clerkId: string,
+  limit: number = 10
+): Promise<Post[]> => {
+  return client.fetch(
+    groq`*[_type == "post" && author->clerkId == $clerkId] {
+      _id,
+      title,
+      "slug": slug.current,
+      excerpt,
+      publishedAt,
+      _createdAt,
+      postType,
+      "imageUrl": image.asset->url,
+      "author": author->{_id, username, "imageUrl": imageUrl, clerkId},
+      "topicSlug": *[_type == "topic" && _id == ^.topic._ref][0].slug.current,
+      upvotes,
+      downvotes,
+      tags
+    } | order(_createdAt desc)[0...$limit]`,
+    { clerkId, limit }
+  );
+};
+
+export default getLatestActivityOfUser;
