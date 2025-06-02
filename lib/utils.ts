8d707d8be1bd713f2111ed98e4e8397fddd95ecb
ptakondrej <ptakondrej@spseplzen.cz>
2025-06-02 22:27:15 +0200

    Format set to en-US

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,11 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(date: Date | string): string {
+export function formatDate(date: Date | string | undefined): string {
+  if (!date) {
+    return "Unknown date";
+  }
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-  return new Date(date).toLocaleDateString(undefined, options);
+  return new Date(date).toLocaleDateString("en-US", options);
 }
