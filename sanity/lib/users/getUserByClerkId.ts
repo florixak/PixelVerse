3c3ceacbfbb2e0f08c274c638029152ef8a77b98
ptakondrej <florixakcz@seznam.cz>
2025-06-02 14:21:40 +0200

    Update getting user by ids

--- sanity/lib/users/getUserByClerkId.ts
+++ sanity/lib/users/getUserByClerkId.ts
@@ -1,8 +1,31 @@
 import { groq } from "next-sanity";
 import { client } from "../client";
+import { User } from "@/sanity.types";
 
-export const getUserByClerkId = async (clerkId: string) => {
-  return client.fetch(groq`*[_type == "user" && clerkId == $clerkId][0]`, {
-    clerkId,
-  });
+export const getUserByClerkId = async (
+  clerkId: string
+): Promise<User | null> => {
+  return client.fetch(
+    groq`*[_type == "user" && clerkId == $clerkId][0] {
+      _id,
+      _createdAt,
+      username,
+      fullName,
+      email,
+      clerkId,
+      imageUrl,
+      "favoriteTopics": favoriteTopics[]-> {
+        _id,
+        title,
+        "slug": slug.current,
+        description
+      },
+      "postCount": count(*[_type == "post" && references(^._id)]),
+      "commentCount": count(*[_type == "comment" && references(^._id)]),
+      "upvotedPosts": *[_type == "vote" && user._ref == ^._id && value > 0].post._ref,
+      "downvotedPosts": *[_type == "vote" && user._ref == ^._id && value < 0].post._ref,
+      isReported
+    }`,
+    { clerkId }
+  );
 };
--- sanity/lib/users/getUserById.ts
+++ sanity/lib/users/getUserById.ts
@@ -1,15 +1,28 @@
 import { groq } from "next-sanity";
 import { client } from "../client";
+import { User } from "@/sanity.types";
 
-const getUserById = async (userId: string) => {
+const getUserById = async (userId: string): Promise<User> => {
   return client.fetch(
     groq`*[_type == "user" && _id == $userId][0] {
       _id,
+      _createdAt,
       username,
-      "imageUrl": imageUrl,
-      createdAt,
-      "postCount": count(*[_type == "post" && author._ref == $userId]),
-      "commentCount": count(*[_type == "comment" && author._ref == $userId])
+      fullName,
+      email,
+      clerkId,
+      imageUrl,
+      "favoriteTopics": favoriteTopics[]-> {
+        _id,
+        title,
+        "slug": slug.current,
+        description
+      },
+      "postCount": count(*[_type == "post" && references(^._id)]),
+      "commentCount": count(*[_type == "comment" && references(^._id)]),
+      "upvotedPosts": *[_type == "vote" && user._ref == ^._id && value > 0].post._ref,
+      "downvotedPosts": *[_type == "vote" && user._ref == ^._id && value < 0].post._ref,
+      isReported
     }`,
     { userId }
   );
