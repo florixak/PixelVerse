2716a21ac934701f944778f5481feda83ced2175
ptakondrej <florixakcz@seznam.cz>
2025-06-02 14:12:01 +0200

    Create post update function

--- actions/postActions.ts
+++ actions/postActions.ts
@@ -3,34 +3,15 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { writeClient } from "@/sanity/lib/client";
 import slugify from "slugify";
-import addUser from "@/sanity/lib/users/addUser";
+import { ensureSanityUser } from "@/lib/user-utils";
 
 export async function createPost(formData: FormData) {
   const user = await currentUser();
   if (!user) throw new Error("Must be logged in");
 
-  const existingUser = await writeClient.fetch(
-    `*[_type == "user" && clerkId == $clerkId][0]`,
-    { clerkId: user.id }
-  );
-
-  let userId = existingUser?._id;
-  if (!existingUser) {
-    console.log("Creating new user in Sanity");
-    const newUser = await addUser({
-      clerkId: user.id,
-      username: user.username || user.firstName || "Anonymous",
-      fullName: user.fullName || "",
-      email: user.emailAddresses[0]?.emailAddress || "",
-      imageUrl: user.imageUrl || "",
-    });
-    userId = newUser._id;
-  } else {
-    console.log("User already exists in Sanity");
-  }
+  const userId = await ensureSanityUser(user);
 
   const topicId = formData.get("topic")?.toString() || "";
-
   const postTitle = formData.get("title")?.toString() || "Untitled Post";
 
   const newPost = await writeClient.create({
@@ -69,4 +50,27 @@ export async function createPost(formData: FormData) {
     ...newPost,
     topicSlug: topic?.slug || "unknown",
   };
+}
+
+export async function updatePost(formData: FormData, postId: string) {
+  const user = await currentUser();
+  if (!user) throw new Error("Must be logged in");
+
+  const userId = await ensureSanityUser(user);
+
+  const postTitle = formData.get("title")?.toString() || "Untitled Post";
+
+  const updatedPost = await writeClient.patch(postId).set({
+    title: postTitle,
+    slug: {
+      _type: "slug",
+      current: slugify(postTitle, { lower: true, strict: true }),
+    },
+    content: formData.get("content")?.toString() || "",
+    excerpt: formData.get("content")?.toString().substring(0, 150) || "",
+    topic: {
+      _type: "reference",
+      _ref: formData.get("topic")?.toString() || "",
+    },
+  });
 }
