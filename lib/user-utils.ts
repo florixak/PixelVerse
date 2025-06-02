215f7efde0164b00eba7d73bd54b2838fcd60950
ptakondrej <florixakcz@seznam.cz>
2025-06-02 14:14:01 +0200

    Add ensure sanity user util function

---
+++ lib/user-utils.ts
@@ -0,0 +1,26 @@
+import { writeClient } from "@/sanity/lib/client";
+import addUser from "@/sanity/lib/users/addUser";
+import { User } from "@clerk/nextjs/server";
+
+export async function ensureSanityUser(user: User) {
+  if (!user) throw new Error("No user provided");
+
+  const existingUser = await writeClient.fetch(
+    `*[_type == "user" && clerkId == $clerkId][0]`,
+    { clerkId: user.id }
+  );
+
+  if (existingUser) {
+    return existingUser._id;
+  }
+
+  const newUser = await addUser({
+    clerkId: user.id,
+    username: user.username || user.firstName || "Anonymous",
+    fullName: user.fullName || "",
+    email: user.emailAddresses[0]?.emailAddress || "",
+    imageUrl: user.imageUrl || "",
+  });
+
+  return newUser._id;
+}
