3c82c373b4a066e48c4ff2104d7747c4c3562c67
ptakondrej <ptakondrej@spseplzen.cz>
2025-06-02 21:40:09 +0200

    Edit user profile page

--- app/(app)/profile/[clerkId]/page.tsx
+++ app/(app)/profile/[clerkId]/page.tsx
@@ -1,8 +1,11 @@
 import PostCard from "@/components/post-card";
+import { Button } from "@/components/ui/button";
+import { formatDate } from "@/lib/utils";
 import getLatestActivityOfUser from "@/sanity/lib/featured/getLatestActivityOfUser";
-import { currentUser } from "@clerk/nextjs/server";
+import { getUserByClerkId } from "@/sanity/lib/users/getUserByClerkId";
+import { Calendar, DotSquare, Ellipsis, Menu } from "lucide-react";
+import Image from "next/image";
 import { notFound } from "next/navigation";
-import { Suspense } from "react";
 
 type ProfilePageProps = {
   params: Promise<{
@@ -20,38 +23,64 @@ const ProfilePage = async ({ params }: ProfilePageProps) => {
   if (!clerkId) {
     notFound();
   }
-  const user = await currentUser();
+  const user = await getUserByClerkId(clerkId);
 
-  if (!user) {
-    notFound();
-  }
+  console.log("User:", user);
+  console.log(user?.createdAt);
 
   return (
-    <div className="p-6 max-w-2xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4">
-        {clerkId === user?.id ? "Your " : `${user.username}'s `} Profile
-      </h1>
-      <div className="flex items-center mb-6">
-        <img
-          src={user.imageUrl || "/default-avatar.png"}
-          alt={`${user.username}'s avatar`}
-          className="w-16 h-16 rounded-full mr-4"
-        />
-        <div>
-          <h2 className="text-xl font-semibold">{user.username}</h2>
+    <section className="flex flex-col p-16">
+      <div className="flex flex-row justify-between items-start w-ful px-32">
+        <div className="flex flex-row items-center">
+          <Image
+            src={user?.imageUrl || "/avatar-default.png"}
+            alt={`${user?.username}'s avatar`}
+            className="w-36 h-36 rounded-full mr-8"
+            width={192}
+            height={192}
+            loading="lazy"
+            placeholder="blur"
+            blurDataURL="/avatar-default.png"
+          />
+          <div className="flex flex-col justify-center">
+            <h1 className="text-4xl font-semibold">{user?.username}</h1>
+            <p
+              className={`${
+                user?.createdAt ? "block" : "hidden"
+              } text-muted-foreground flex items-center text-base gap-1`}
+              title={`Joined on ${formatDate(user?.createdAt)}`}
+            >
+              <Calendar size={16} /> Joined on {formatDate(user?.createdAt)}
+            </p>
+            <div className="flex flex-row justify-end gap-4">
+              <div className="flex flex-col items-end">
+                <p className="text-2xl font-semibold">{user?.postCount || 0}</p>
+                <p className="text-muted-foreground">Posts</p>
+              </div>
+              <div className="flex flex-col items-end">
+                <p className="text-2xl font-semibold">
+                  {user?.commentCount || 0}
+                </p>
+                <p className="text-muted-foreground">Comments</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center gap-2 mt-5">
+          <Button>Follow</Button>
+          <Button variant="outline">
+            <Ellipsis />
+          </Button>
         </div>
       </div>
-
-      <div>
-        <h2>Latest activity</h2>
-        <p className="text-gray-600">
-          This section will show the latest activity of {user.username}.
+      <div className="flex flex-col mt-8 px-32">
+        <h2 className="text-2xl font-semibold">Latest Activity</h2>
+        <p className="text-muted-foreground">
+          Here are the latest posts and comments made by {user?.username}.
         </p>
-        <Suspense fallback={<div>Loading latest activity...</div>}>
-          <LatestUserActivity clerkId={clerkId} limit={6} />
-        </Suspense>
+        <LatestUserActivity clerkId={clerkId} limit={8} />
       </div>
-    </div>
+    </section>
   );
 };
 
@@ -62,10 +91,16 @@ const LatestUserActivity = async ({
   const posts = await getLatestActivityOfUser(clerkId, limit);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 w-full">
-      {posts.map((post) => (
-        <PostCard key={post._id} post={post} className="w-full" />
-      ))}
+    <div className="columns-1 sm:columns-2 md:columns-3 gap-4 mt-6 space-y-4">
+      {posts && posts.length > 0 ? (
+        posts.map((post) => (
+          <div key={post._id} className="break-inside-avoid mb-4">
+            <PostCard post={post} />
+          </div>
+        ))
+      ) : (
+        <div className="text-gray-500">No posts found for this topic.</div>
+      )}
     </div>
   );
 };
