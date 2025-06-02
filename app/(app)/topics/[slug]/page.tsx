f9954036c0c20d3e9a6306490f45dee541e4070d
ptakondrej <florixakcz@seznam.cz>
2025-06-02 14:18:09 +0200

    Update topic page and posts

--- app/(app)/topics/[slug]/page.tsx
+++ app/(app)/topics/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import PostCard from "@/components/post-card";
 import getPostsByTopic from "@/sanity/lib/posts/getPostsByTopic";
 import getTopicBySlug from "@/sanity/lib/topics/getTopicBySlug";
 import React, { Suspense } from "react";
@@ -19,44 +20,34 @@ const TopicPage = async ({ params }: TopicPageProps) => {
       </div>
     );
   }
-  const posts = await getPostsByTopic(slug);
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Topic: {topic.title}</h1>
+      <h1 className="text-2xl font-bold mb-4">{topic.title}</h1>
       <p className="text-gray-700">
-        This is the page for the topic: <strong>{slug}</strong>. Here you can
-        find posts related to this topic.
+        {topic.description || "No description available for this topic."}
       </p>
       <Suspense fallback={<div>Loading posts...</div>}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-6">
-          {posts && posts.length > 0 ? (
-            posts.map((post) => (
-              <div
-                key={post.slug}
-                className="p-4 border rounded-lg hover:shadow-lg transition-shadow"
-              >
-                <h2 className="text-xl font-semibold">{post.title}</h2>
-                <p className="text-sm text-gray-500">
-                  Created on: {new Date(post._createdAt).toLocaleDateString()}
-                </p>
-                <p className="text-sm text-gray-500">
-                  Last modified:{" "}
-                  {new Date(post._updatedAt).toLocaleDateString()}
-                </p>
-                <a
-                  href={`/topics/${topic.slug}/${post.slug}`}
-                  className="text-blue-600 hover:underline mt-2 inline-block"
-                >
-                  View Post
-                </a>
-              </div>
-            ))
-          ) : (
-            <div className="text-gray-500">No posts found for this topic.</div>
-          )}
-        </div>
+        <Posts slug={slug} />
       </Suspense>
+    </div>
+  );
+};
+
+const Posts = async ({ slug }: { slug: string }) => {
+  const posts = await getPostsByTopic(slug);
+
+  return (
+    <div className="columns-1 sm:columns-2 md:columns-3 gap-4 mt-6 space-y-4">
+      {posts && posts.length > 0 ? (
+        posts.map((post) => (
+          <div key={post._id} className="break-inside-avoid mb-4">
+            <PostCard post={post} />
+          </div>
+        ))
+      ) : (
+        <div className="text-gray-500">No posts found for this topic.</div>
+      )}
     </div>
   );
 };
