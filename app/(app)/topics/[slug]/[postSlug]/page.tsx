fe05330b2f3f8a0557d8f26a8666bb6a497c8c80
ptakondrej <ptakondrej@spseplzen.cz>
2025-06-03 16:23:09 +0200

    Remake of user profile

--- app/(app)/topics/[slug]/[postSlug]/page.tsx
+++ app/(app)/topics/[slug]/[postSlug]/page.tsx
@@ -1,18 +1,67 @@
-import React from "react";
+import PostAuthorButtons from "@/components/post-author-buttons";
+import PostReactions from "@/components/post-reactions";
+import { formatDate } from "@/lib/utils";
+import getPostBySlug from "@/sanity/lib/posts/getPostBySlug";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 const PostPage = async ({
   params,
 }: {
-  params: Promise<{ slug: string; postSlug: string }>;
+  params: Promise<{ postSlug: string }>;
 }) => {
-  const { slug, postSlug } = await params;
+  const { postSlug } = await params;
+  const user = await currentUser();
+  const post = await getPostBySlug(postSlug, user?.id);
+  if (!post) {
+    notFound();
+  }
+
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Post: {postSlug}</h1>
-      <p className="text-gray-700">
-        This is the content of the post with slug: {postSlug} in topic: {slug}.
-      </p>
-      {/* Here you can add more details about the post, like content, author, etc. */}
+    <div className="relative p-6 flex-center w-full flex-col">
+      {post.author?.clerkId === user?.id && (
+        <PostAuthorButtons postId={post._id} />
+      )}
+      <div className="max-w-2xl w-full">
+        <h1 className="text-3xl font-bold">
+          {post.title}
+          {post.isOriginal && (
+            <span
+              className="ml-2 px-2 py-0.5 rounded bg-blue-100 text-blue-700 text-xs font-medium"
+              title="This post is marked as an original creation by the author"
+            >
+              Original
+            </span>
+          )}
+        </h1>
+
+        <p className="text-gray-500 mb-2">
+          {post.author?.username} on{" "}
+          {formatDate(post.publishedAt || post._createdAt)}
+        </p>
+        <p>{post.excerpt || "No excerpt available for this post."}</p>
+      </div>
+
+      <div className="max-w-2xl w-full mt-6">
+        {post.imageUrl && (
+          <Image
+            src={post.imageUrl}
+            alt={post.title || "Post Image"}
+            className="w-full rounded-lg mt-4"
+            width={800}
+            height={450}
+            loading="lazy"
+            placeholder="blur"
+            blurDataURL={post.imageUrl}
+          />
+        )}
+        <PostReactions
+          postId={post._id}
+          reactions={post.reactions || []}
+          currentUserClerkId={user?.id || ""}
+        />
+      </div>
     </div>
   );
 };
