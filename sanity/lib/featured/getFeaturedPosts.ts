9142ce5014a0c41548244911863dba2b6814ec14
ptakondrej <florixakcz@seznam.cz>
2025-06-03 17:19:39 +0200

    Fix getting featured posts

--- sanity/lib/featured/getFeaturedPosts.ts
+++ sanity/lib/featured/getFeaturedPosts.ts
@@ -3,21 +3,28 @@ import { client } from "../client";
 import { Post } from "@/sanity.types";
 
 export const getFeaturedPosts = async (): Promise<Post[]> => {
-  return client.fetch(
+  return client.fetch<Post[]>(
     groq`
       *[_type == "post" && isDeleted != true] | order(upvotes desc)[0...6] {
         _id,
-        title,
-        "slug": slug.current,
-        excerpt,
-        postType,
-        "imageUrl": image.asset->url,
-        "author": author->{username, "imageUrl": imageUrl, clerkId},
-        "topic": topic->{title, "slug": slug.current},
-        "comments": *[_type == "comment" && references(^._id)]{
+      title,
+      "slug": slug.current,
+      excerpt,
+      publishedAt,
+      postType,
+      "imageUrl": image.asset->url,
+      "author": author->{_id, username, "imageUrl": imageUrl, clerkId},
+      "topicSlug": *[_type == "topic" && _id == ^.topic._ref][0].slug.current,
+      dimensions,
+      software,
+      "likes": count(reactions[type == "like"]),
+      "dislikes": count(reactions[type == "dislike"]),
+      tags,
+      isDeleted,
+      "comments": *[_type == "comment" && references(^._id)]{
         _id,
         content,
-        author->{_id, username, "imageUrl": imageUrl},
+        author->{_id, username, "imageUrl": imageUrl, clerkId},
         publishedAt,
         parentComment,
         likes,
