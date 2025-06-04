db11063b1739c3db61d8abb61f1324f1c699b4a6
ptakondrej <florixakcz@seznam.cz>
2025-06-04 12:48:36 +0200

    Change style and props

--- app/(app)/topics/[slug]/[postSlug]/page.tsx
+++ app/(app)/topics/[slug]/[postSlug]/page.tsx
@@ -1,11 +1,8 @@
 import PostComments from "@/components/post/post-comments";
 import PostContent from "@/components/post/post-content";
 import PostHeader from "@/components/post/post-header";
-import PostReactions from "@/components/post/post-reactions";
-import { formatDate } from "@/lib/utils";
 import getPostBySlug from "@/sanity/lib/posts/getPostBySlug";
 import { currentUser } from "@clerk/nextjs/server";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
@@ -17,12 +14,13 @@ const PostPage = async ({
   const { postSlug } = await params;
   const user = await currentUser();
   const post = await getPostBySlug(postSlug, user?.id);
+
   if (!post) {
     notFound();
   }
 
   return (
-    <div className="relative max-w-4xl mx-auto flex-col">
+    <section className="relative max-w-4xl mx-auto flex flex-col gap-4">
       <div className="flex flex-col gap-2 border border-muted rounded-lg p-4 bg-background">
         <PostHeader post={post} isAuthor={post.author?.clerkId === user?.id} />
 
@@ -30,9 +28,9 @@ const PostPage = async ({
       </div>
 
       <Suspense fallback={<p>Loading comments...</p>}>
-        <PostComments postId={post._id} />
+        <PostComments post={post} />
       </Suspense>
-    </div>
+    </section>
   );
 };
 
