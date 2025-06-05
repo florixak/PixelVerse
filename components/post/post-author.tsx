8d27ed6fcba495290a8f65a67796ecf154ea8dbe
ptakondrej <florixakcz@seznam.cz>
2025-06-05 11:38:42 +0200

    Expand post author functionality with props

--- components/post/post-author.tsx
+++ components/post/post-author.tsx
@@ -1,4 +1,4 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { Post } from "@/sanity.types";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,34 +6,51 @@ import Link from "next/link";
 type PostAuthorProps = {
   author: Post["author"];
   publishedAt?: Post["publishedAt"];
+  hideImage?: boolean;
+  hideUsername?: boolean;
+  hideFullName?: boolean;
+  className?: string;
+  imageClassName?: string;
 };
 
-const PostAuthor = ({ author, publishedAt }: PostAuthorProps) => {
+const PostAuthor = ({
+  author,
+  publishedAt,
+  hideImage = false,
+  hideUsername = false,
+  hideFullName = false,
+  className,
+  imageClassName,
+}: PostAuthorProps) => {
   return (
-    <div className="flex flex-row gap-4 items-center">
-      <Link href={`/profile/${author?.clerkId}`} className="hidden sm:block">
-        <Image
-          src={author?.imageUrl || "/avatar-default.svg"}
-          alt={author?.username || "Author Image"}
-          className="w-12 h-12 rounded-full"
-          width={48}
-          height={48}
-          loading="lazy"
-          placeholder="blur"
-          blurDataURL={author?.imageUrl || "/avatar-default.svg"}
-        />
-      </Link>
+    <div className={cn("flex flex-row gap-4 items-center", className)}>
+      {!hideImage && (
+        <Link href={`/profile/${author?.clerkId}`} className="hidden sm:block">
+          <Image
+            src={author?.imageUrl || "/avatar-default.svg"}
+            alt={author?.username || "Author Image"}
+            className={cn("w-12 h-12 rounded-full", imageClassName)}
+            width={48}
+            height={48}
+            loading="lazy"
+            placeholder="blur"
+            blurDataURL={author?.imageUrl || "/avatar-default.svg"}
+          />
+        </Link>
+      )}
       <div className="flex flex-col">
-        <h2 className="text-lg font-semibold">
-          <Link
-            href={`/profile/${author?.clerkId}`}
-            className="hover:underline"
-          >
-            {author?.fullName || author?.username || "Unknown Author"}
-          </Link>
-        </h2>
+        {!hideFullName && (
+          <h2 className="text-lg font-semibold">
+            <Link
+              href={`/profile/${author?.clerkId}`}
+              className="hover:underline"
+            >
+              {author?.fullName || author?.username || "Unknown Author"}
+            </Link>
+          </h2>
+        )}
         <div className="text-sm text-muted-foreground">
-          <p>@{author?.username || "Unknown"}</p>
+          {!hideUsername && <p>@{author?.username || "Unknown"}</p>}
           <p>{formatDate(publishedAt)}</p>
         </div>
       </div>
