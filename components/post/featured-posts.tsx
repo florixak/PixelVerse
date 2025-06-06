942b85bfdd7d1bf0f869275eea84604a48a75b38
ptakondrej <florixakcz@seznam.cz>
2025-06-06 06:29:00 +0200

    Cleaned featured posts

--- components/post/featured-posts.tsx
+++ components/post/featured-posts.tsx
@@ -1,23 +1,10 @@
 import { getFeaturedPosts } from "@/sanity/lib/featured/getFeaturedPosts";
-import PostCard from "./post-card";
 
-type FeaturedPostsProps = {
-  className?: string;
-  showTitle?: boolean;
-  title?: string;
-};
+import Posts from "./posts";
 
-const FeaturedPosts = async ({ className = "" }: FeaturedPostsProps) => {
+const FeaturedPosts = async () => {
   const posts = await getFeaturedPosts();
-  return (
-    <div className={`featured-posts ${className}`}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-        {posts.map((post) => (
-          <PostCard key={post._id} post={post} />
-        ))}
-      </div>
-    </div>
-  );
+  return <Posts posts={posts} />;
 };
 
 export default FeaturedPosts;
