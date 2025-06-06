e9a1cf418570a950d0bd9609fd8259ba2acc910a
ptakondrej <florixakcz@seznam.cz>
2025-06-06 06:29:53 +0200

    Add new orders for filtering

--- sanity/lib/topics/getAllTopics.ts
+++ sanity/lib/topics/getAllTopics.ts
@@ -4,7 +4,7 @@ import { groq } from "next-sanity";
 
 type GetAllTopicsParams = {
   limit?: number;
-  order?: "alphabetical" | "latest";
+  order?: "alphabetical" | "latest" | "oldest" | "popular" | "trending";
   from?: number;
 };
 
@@ -13,19 +13,44 @@ const getAllTopics = async ({
   order = "alphabetical",
   from = 0,
 }: GetAllTopicsParams): Promise<Topic[]> => {
+  let sortCriteria = "";
+
+  switch (order) {
+    case "alphabetical":
+      sortCriteria = "order(title asc)";
+      break;
+    case "latest":
+      sortCriteria = "order(_createdAt desc)";
+      break;
+    case "oldest":
+      sortCriteria = "order(_createdAt asc)";
+      break;
+    case "trending":
+      sortCriteria =
+        "order(count(*[_type == 'post' && references(^._id) && isDeleted != true]) desc)";
+      break;
+    case "popular":
+      const sevenDaysAgo = new Date();
+      sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+      const recentTimestamp = sevenDaysAgo.toISOString();
+
+      sortCriteria = `order(count(*[_type == 'post' && references(^._id) && _createdAt > '${recentTimestamp}' && isDeleted != true]) desc)`;
+      break;
+    default:
+      sortCriteria = "order(title asc)";
+  }
+
   return client.fetch(
-    groq`*[_type == "topic"] | order(${
-      order === "alphabetical" ? "title" : "_createdAt"
-    } ${order === "latest" ? "desc" : "asc"})[${from}...${from + limit}] {
+    groq`*[_type == "topic"] | ${sortCriteria}[${from}...${from + limit}] {
       _id,
       title,
       "slug": slug.current,
       description,
       publishedAt,
       updatedAt,
-        "iconUrl": icon.asset->url,
-        "bannerUrl": banner.asset->url,
-        "postCount": count(*[_type == "post" && references(^._id) && isDeleted != true]),
+      "iconUrl": icon.asset->url,
+      "bannerUrl": banner.asset->url,
+      "postCount": count(*[_type == "post" && references(^._id) && isDeleted != true]),
       "comments": *[
         _type == "comment" &&
         references(*[_type == "post" && references(^._id) && isDeleted != true]._id)
