bf4b5b3ce279bc6e3bb59ae92f49f0bec86f17f3
ptakondrej <florixakcz@seznam.cz>
2025-06-07 11:39:49 +0200

    Add new sort order

--- lib/types.ts
+++ lib/types.ts
@@ -1,15 +1,6 @@
-export type Topic = {
-  title: string;
-  slug: string;
-  lastModified: Date;
-  createdAt: Date;
-  posts?: {
-    title: string;
-    slug: string;
-    content: string;
-    createdAt: Date;
-    lastModified: Date;
-  }[];
-};
-
-export type SortOrder = "latest" | "oldest" | "alphabetical";
+export type SortOrder =
+  | "latest"
+  | "oldest"
+  | "alphabetical"
+  | "popular"
+  | "trending";
