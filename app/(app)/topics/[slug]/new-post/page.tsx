96b33279c3feec7550bcef792b6501ecbc478606
ptakondrej <florixakcz@seznam.cz>
2025-06-07 11:38:41 +0200

    Add topic slug to form

--- app/(app)/topics/[slug]/new-post/page.tsx
+++ app/(app)/topics/[slug]/new-post/page.tsx
@@ -1,10 +1,19 @@
 import CreatePostForm from "@/components/post-form/create-post-form";
 import getAllTopics from "@/sanity/lib/topics/getAllTopics";
 
-const PostCreatePage = async () => {
-  const topics = await getAllTopics({});
+type PostCreatePageProps = {
+  params: Promise<{ slug: string }>;
+};
 
-  return <CreatePostForm topics={topics} />;
+const PostCreatePage = async ({ params }: PostCreatePageProps) => {
+  const topics = await getAllTopics({});
+  if (!topics || topics.length === 0) {
+    return <p>No topics available. Please create a topic first.</p>;
+  }
+
+  const { slug } = await params;
+
+  return <CreatePostForm topics={topics} topicSlug={slug} />;
 };
 
 export default PostCreatePage;
