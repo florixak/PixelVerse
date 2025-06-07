375417ce065697e839065f39a0d7e65d123aec57
ptakondrej <florixakcz@seznam.cz>
2025-06-07 11:46:42 +0200

    Create role component

---
+++ components/role.tsx
@@ -0,0 +1,21 @@
+import { User } from "@/sanity.types";
+import { Badge } from "./ui/badge";
+
+type RoleProps = {
+  role: User["role"];
+};
+
+const Role = ({ role }: RoleProps) => {
+  return (
+    <Badge
+      className={
+        "text-xs font-semibold px-2 py-1 rounded-md bg-gray-200 text-gray-800"
+      }
+    >
+      {role === "admin" && "Admin"}
+      {role === "moderator" && "Mod"}
+    </Badge>
+  );
+};
+
+export default Role;
