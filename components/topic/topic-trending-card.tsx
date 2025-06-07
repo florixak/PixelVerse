aad2810f30a0f74a1ad010b93cb94df5e62c2243
ptakondrej <florixakcz@seznam.cz>
2025-06-07 11:49:59 +0200

    Create topic trending card

---
+++ components/topic/topic-trending-card.tsx
@@ -0,0 +1,40 @@
+import { Topic } from "@/sanity.types";
+import Card from "../card";
+import Image from "next/image";
+import Link from "next/link";
+
+type TopicTrendingCardProps = {
+  topic: Topic;
+  className?: string;
+};
+
+const TopicTrendingCard = ({ topic, className }: TopicTrendingCardProps) => {
+  return (
+    <Card className={`flex flex-row items-center p-2 w-[13rem] ${className}`}>
+      <div className="flex items-center justify-center min-w-[80px] h-16 p-2">
+        {topic.iconUrl && (
+          <Image
+            src={topic.iconUrl}
+            alt={`Icon for ${topic.title}`}
+            width={48}
+            height={48}
+            className="object-contain rounded-lg"
+            loading="lazy"
+            blurDataURL={topic.iconUrl}
+          />
+        )}
+      </div>
+      <div className="flex-1">
+        <Link href={`/topics/${topic.slug}`}>
+          <h2 className="text-xl font-bold mt-2">{topic.title}</h2>
+        </Link>
+
+        <p className="text-sm text-muted-foreground mt-2">
+          {topic.postCount} posts
+        </p>
+      </div>
+    </Card>
+  );
+};
+
+export default TopicTrendingCard;
