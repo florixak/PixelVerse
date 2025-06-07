9b08bbe184fa4f4e9edeca31837acd794d3d6780
ptakondrej <florixakcz@seznam.cz>
2025-06-07 11:42:24 +0200

    Add className prop

--- components/masonry-wrapper.tsx
+++ components/masonry-wrapper.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils";
+
 type MasonryWrapperProps = {
   children?: React.ReactNode;
+  className?: string;
 };
 
-const MasonryWrapper = ({ children }: MasonryWrapperProps) => {
+const MasonryWrapper = ({ children, className }: MasonryWrapperProps) => {
   return (
-    <div className="columns-1 sm:columns-2 md:columns-3 gap-4 space-y-4">
+    <div
+      className={cn(
+        "columns-1 sm:columns-2 md:columns-3 gap-4 space-y-4",
+        className
+      )}
+    >
       {children}
     </div>
   );
