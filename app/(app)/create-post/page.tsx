f78ce9c9efed09fdfead74eff76d488ae80a02f8
ptakondrej <florixakcz@seznam.cz>
2025-06-09 10:47:21 +0200

    Create new create post page

---
+++ app/(app)/create-post/page.tsx
@@ -0,0 +1,29 @@
+import CreatePostForm from "@/components/post-form/create-post-form";
+import getAllTopics from "@/sanity/lib/topics/getAllTopics";
+import getTopicBySlug from "@/sanity/lib/topics/getTopicBySlug";
+
+type CreatePostPageProps = {
+  searchParams: Promise<{
+    topic: string;
+  }>;
+};
+
+const CreatePostPage = async ({ searchParams }: CreatePostPageProps) => {
+  const { topic: topicSlug } = await searchParams;
+  const topics = await getAllTopics({});
+  const topic = await getTopicBySlug(decodeURIComponent(topicSlug));
+
+  return (
+    <section className="flex-center flex-col gap-3 px-0 py-6 md:p-6 w-full">
+      <div className="space-y-2 text-center">
+        <h1 className="text-3xl font-bold">Create a New Post</h1>
+        <p className="text-muted-foreground">
+          Share your pixel art, tutorial, or question with the community
+        </p>
+      </div>
+      <CreatePostForm topics={topics} topic={topic} />
+    </section>
+  );
+};
+
+export default CreatePostPage;
