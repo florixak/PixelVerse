f95f06b9f0459328d5a777f05157ca8ecdfdcbf0
ptakondrej <florixakcz@seznam.cz>
2025-06-09 10:47:42 +0200

    Create sorting component

---
+++ components/sort-filter-select.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { useRouter } from "next/navigation";
+import { SortOrder } from "@/lib/types";
+
+const SortFilterSelect = () => {
+  const router = useRouter();
+
+  const handleSortChange = (value: SortOrder) => {
+    if (!value) return;
+    const url = new URL(window.location.href);
+    url.searchParams.set("sort", value);
+    router.push(url.toString());
+  };
+
+  return (
+    <Select onValueChange={handleSortChange} defaultValue="latest">
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder="Sort by" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="latest">Latest</SelectItem>
+        <SelectItem value="oldest">Oldest</SelectItem>
+        <SelectItem value="alphabetical">Alphabetical</SelectItem>
+        <SelectItem value="popular">Popular</SelectItem>
+        <SelectItem value="trending">Trending</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SortFilterSelect;
