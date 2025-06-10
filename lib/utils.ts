3e25c5a146cd1e8866697e32630f5d01126d8d75
ptakondrej <florixakcz@seznam.cz>
2025-06-10 10:42:01 +0200

    Add time options

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,14 +5,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(date: Date | string | undefined): string {
+export function formatDate(
+  date: Date | string | undefined,
+  options?: { showTime?: boolean }
+): string {
   if (!date) {
     return "Unknown date";
   }
-  const options: Intl.DateTimeFormatOptions = {
+  const timeOptions: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
+    ...(options?.showTime && {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }),
   };
-  return new Date(date).toLocaleDateString("en-US", options);
+  return new Date(date).toLocaleDateString("en-US", timeOptions);
 }
