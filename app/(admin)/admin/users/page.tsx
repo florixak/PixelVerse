f4cc22543ad69b4137f88f22bc90e1820ded2af5
ptakondrej <florixakcz@seznam.cz>
2025-06-10 10:43:26 +0200

    Create admin dashboard page

---
+++ app/(admin)/admin/users/page.tsx
@@ -0,0 +1,20 @@
+import AdminUsersClient from "@/components/admin/admin-users-client";
+import getAllUsers from "@/sanity/lib/users/getAllUsers";
+import { getUserByClerkId } from "@/sanity/lib/users/getUserByClerkId";
+import { currentUser } from "@clerk/nextjs/server";
+
+const AdminUsersPage = async () => {
+  const users = await getAllUsers();
+  const user = await currentUser();
+  if (!user) {
+    return <div>You must be logged in to view this page.</div>;
+  }
+  const sanityUser = await getUserByClerkId(user.id);
+  return (
+    <section>
+      <AdminUsersClient initialUsers={users} user={sanityUser} />
+    </section>
+  );
+};
+
+export default AdminUsersPage;
