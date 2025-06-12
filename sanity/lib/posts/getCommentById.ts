c5a9dfbcb2adad7b15ceb8ecccc704a4e80ec558
ptakondrej <florixakcz@seznam.cz>
2025-06-12 13:03:52 +0200

    Create getCommentById

---
+++ sanity/lib/posts/getCommentById.ts
@@ -0,0 +1,34 @@
+import { Comment } from "@/sanity.types";
+import { client } from "../client";
+import { groq } from "next-sanity";
+
+export const getCommentById = async (commentId: string): Promise<Comment> => {
+  return client.fetch<Comment>(
+    groq`*[_type == "comment" && _id == $commentId && author->isBanned != true][0] {
+      _id,
+      content,
+      author->{_id, username, "imageUrl": imageUrl, clerkId, role, isBanned},
+      post->{
+        _id, 
+        title, 
+        "slug": slug.current,
+        excerpt,
+        postType,
+        "author": author->{_id, username, "imageUrl": imageUrl},
+        "topicSlug": topic->slug.current,
+        "topic": topic->{_id, "slug": slug.current, title},
+        publishedAt,
+        "commentsCount": count(*[_type == "comment" && references(^._id)])
+      },
+      publishedAt,
+      parentComment,
+      likes,
+      dislikes,
+      isEdited,
+      lastEditedAt,
+      pixelArtUrl,
+      isDeleted
+    }`,
+    { commentId }
+  );
+};
