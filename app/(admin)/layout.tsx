fe2c41cccc453cbf03d03f2c9a411206cc1c8e0b
ptakondrej <florixakcz@seznam.cz>
2025-06-12 12:20:11 +0200

    Update admin layout

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,4 +1,5 @@
 import AdminSidebar from "@/components/admin-sidebar";
+import Header from "@/components/header";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { canAccessDashboard } from "@/lib/user-utils";
 import { getUserByClerkId } from "@/sanity/lib/users/getUserByClerkId";
@@ -23,29 +24,17 @@ export default async function AdminLayout({
 
   return (
     <>
-      <div className="bg-primary/10 p-2 text-primary flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <span className="font-bold">PixelVerse Admin</span>
-        </div>
-        <a href="/" className="text-sm hover:underline">
-          Return to App
-        </a>
-      </div>
-
-      {/*<AdminCommandSearch />*/}
-
-      <div className="flex h-[calc(100vh-40px)]">
-        <SidebarProvider defaultOpen={true}>
-          <AdminSidebar />
-          <main className="relative min-h-screen w-full px-4 mt-6">
-            <SidebarTrigger
-              className="fixed top-3 z-40 size-10 rounded-md bg-background border shadow-sm hover:bg-muted transition-colors duration-200"
-              aria-label="Toggle sidebar"
-            />
-            {children}
-          </main>
-        </SidebarProvider>
-      </div>
+      <Header />
+      <SidebarProvider defaultOpen={true}>
+        <AdminSidebar />
+        <main className="relative min-h-screen w-full px-4 mt-6">
+          <SidebarTrigger
+            className="fixed top-3 z-40 size-10 rounded-md bg-background border shadow-sm hover:bg-muted transition-colors duration-200"
+            aria-label="Toggle sidebar"
+          />
+          {children}
+        </main>
+      </SidebarProvider>
     </>
   );
 }
