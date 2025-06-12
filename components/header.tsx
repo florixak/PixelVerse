2a51734132e91bae40050e0c99b730b58deb3662
ptakondrej <florixakcz@seznam.cz>
2025-06-12 12:20:54 +0200

    Update header styles

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import AuthButtons from "./auth-buttons";
 
 const Header = () => {
   return (
-    <header className="flex justify-end items-center p-4 gap-4 h-16 bg-muted/50 border-b border-muted/20">
+    <header className="flex justify-end items-center p-4 gap-4 h-16 border-b border-muted/20 w-full z-50 bg-background dark:bg-background/50 backdrop-blur-xs">
       <AuthButtons />
       <ThemeSwitcher />
     </header>
