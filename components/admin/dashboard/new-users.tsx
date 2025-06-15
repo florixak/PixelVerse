498eed365eb331e3290dd95f00e81d773a17903c
ptakondrej <florixakcz@seznam.cz>
2025-06-15 15:29:33 +0200

    Remove redundant div

--- components/admin/dashboard/new-users.tsx
+++ components/admin/dashboard/new-users.tsx
@@ -5,13 +5,10 @@ const NewUsers = async () => {
   const newUsers = await getRecentUsers(6);
 
   return (
-    <div className="space-y-1">
-      <h3 className="text-sm font-medium mb-2">New Users</h3>
-      <div className="space-y-1.5">
-        {newUsers.map((user) => (
-          <CompactUserCard key={user._id} user={user} />
-        ))}
-      </div>
+    <div className="space-y-1.5">
+      {newUsers.map((user) => (
+        <CompactUserCard key={user._id} user={user} />
+      ))}
     </div>
   );
 };
