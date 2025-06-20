23816a2a2241e412618a15b3f36868035278ac60
ptakondrej <florixakcz@seznam.cz>
2025-06-20 08:03:09 +0200

    Create infinite scroll hook

---
+++ hooks/use-infinite-scroll.ts
@@ -0,0 +1,60 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+type GetLatestPostsParams<T> = {
+  page?: number;
+  limit: number;
+  queryFn: (params: { page: number; limit: number }) => Promise<T>;
+  queryKey: string[];
+};
+
+const useInfiniteScroll = <T>({
+  queryFn,
+  limit = 8,
+  page,
+  queryKey,
+}: GetLatestPostsParams<T>) => {
+  const {
+    data,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+  } = useInfiniteQuery({
+    queryKey: queryKey,
+    queryFn: async ({ pageParam = 0 }) =>
+      await queryFn({ page: pageParam, limit: limit }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (!lastPage || !Array.isArray(lastPage)) return undefined;
+      if (!lastPage || lastPage.length < limit) return undefined;
+      return allPages.length;
+    },
+  });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+  return {
+    data,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    ref, // Ref to attach to the sentinel element
+    inView, // Whether the sentinel is in view
+    limit, // The limit used for pagination
+  };
+};
+
+export default useInfiniteScroll;
