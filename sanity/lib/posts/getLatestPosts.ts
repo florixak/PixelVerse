63a0fd90dd7898471376cd2bd0a46505353346ac
ptakondrej <florixakcz@seznam.cz>
2025-06-20 18:53:15 +0200

    Change init limit

--- sanity/lib/posts/getLatestPosts.ts
+++ sanity/lib/posts/getLatestPosts.ts
@@ -1,7 +1,6 @@
-import { NEWEST_POSTS_LIMIT } from "@/components/landing-page/newest-posts";
 import getAllPosts from "./getAllPosts";
 
-export async function getLatestPosts({ page = 0, limit = NEWEST_POSTS_LIMIT }) {
+export async function getLatestPosts({ page = 0, limit = 8 }) {
   const posts = await getAllPosts({
     sort: "latest",
     limit,
