2363453aeb41faa7e7dbf010d045ded478b82a5d
ptakondrej <ptakondrej@spseplzen.cz>
2025-06-30 10:07:39 +0200

    Add suspense for post header

--- app/(app)/topics/[slug]/[postSlug]/page.tsx
+++ app/(app)/topics/[slug]/[postSlug]/page.tsx
@@ -22,7 +22,12 @@ const PostPage = async ({
   return (
     <section className="relative max-w-4xl mx-auto flex flex-col gap-4">
       <div className="flex flex-col gap-2 border border-muted rounded-lg p-4 bg-background">
-        <PostHeader post={post} isAuthor={post.author?.clerkId === user?.id} />
+        <Suspense fallback={<p>Loading post...</p>}>
+          <PostHeader
+            post={post}
+            isAuthor={post.author?.clerkId === user?.id}
+          />
+        </Suspense>
 
         <PostContent post={post} userId={user?.id} />
       </div>
