6232e274b86d6a9e8f7d2f86c3eb79660417ac64
ptakondrej <florixakcz@seznam.cz>
2025-07-17 13:49:40 +0200

    Restrict admin layout to non-production environments
    Added a check to render a 404 page when accessing the admin layout in production using Next.js's notFound(). This helps prevent unauthorized access to admin routes in production.

--- app/(sanity)/layout.tsx
+++ app/(sanity)/layout.tsx
@@ -1,7 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  if (process.env.NODE_ENV === "production") {
+    notFound();
+  }
+
   return children;
 }
