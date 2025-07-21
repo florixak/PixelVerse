66995b494ac681ae06fcea692a583c9412edac1d
ptakondrej <florixakcz@seznam.cz>
2025-07-21 09:34:58 +0200

    Enhance sidebar header with logo and responsive layout
    Added PixelVerse logo using next/image and updated the sidebar header to show or hide the title based on sidebar state and device type. Improved accessibility with aria-label and adjusted styles for better responsiveness.

--- components/sidebar-header.tsx
+++ components/sidebar-header.tsx
@@ -1,13 +1,32 @@
+"use client";
+
 import Link from "next/link";
-import { SidebarHeader as SidebarHead } from "@/components/ui/sidebar";
+import {
+  SidebarHeader as SidebarHead,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
 
 const SidebarHeader = () => {
+  const { open, isMobile } = useSidebar();
   return (
     <SidebarHead className="flex items-center justify-center gap-2 p-4 overflow-hidden">
-      <Link href="/">
-        <h1 className="text-2xl font-bold overflow-hidden">
-          <span className="text-primary">Pixel</span>Verse
-        </h1>
+      <Link
+        href="/"
+        className={open || isMobile ? "flex items-center gap-2" : "scale-200"}
+        aria-label="PixelVerse Home"
+      >
+        <Image
+          src="/pixelverse-favicon.png"
+          alt="PixelVerse Logo"
+          width={56}
+          height={56}
+        />
+        {(open || isMobile) && (
+          <h1 className="text-4xl font-bold overflow-hidden font-pixel">
+            <span className="text-primary">Pixel</span>Verse
+          </h1>
+        )}
       </Link>
     </SidebarHead>
   );
