68453d18e24e6f69557cc13119049fed9841eef3
ptakondrej <florixakcz@seznam.cz>
2025-07-21 08:15:37 +0200

    Pass userId to report form

--- app/(admin)/admin/reports/[id]/page.tsx
+++ app/(admin)/admin/reports/[id]/page.tsx
@@ -2,6 +2,7 @@ import AdminReportDetails from "@/components/admin/admin-report-details";
 import AdminReportForm from "@/components/admin/admin-report-form";
 import { Report } from "@/sanity.types";
 import { getReportById } from "@/sanity/lib/reports/getReportById";
+import { auth } from "@clerk/nextjs/server";
 import { notFound } from "next/navigation";
 
 type ReportDetailsPageProps = {
@@ -14,10 +15,14 @@ const ReportDetailsPage = async ({ params }: ReportDetailsPageProps) => {
   if (!report) {
     notFound();
   }
+  const { userId } = await auth();
+  if (!userId) {
+    notFound();
+  }
   return (
     <section className="flex flex-col gap-8 w-full max-w-4xl mx-auto p-6">
       <AdminReportDetails report={report} />
-      <AdminReportForm report={report} />
+      <AdminReportForm report={report} userId={userId} />
     </section>
   );
 };
