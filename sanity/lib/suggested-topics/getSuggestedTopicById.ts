32b7f3119e96135449893190357b946c7184edde
ptakondrej <florixakcz@seznam.cz>
2025-08-05 13:59:12 +0200

    Add suggested topics query functions
    Introduces getAllSuggestedTopics and getSuggestedTopicById functions to fetch suggested topics from Sanity. These utilities support retrieving all topics and fetching a topic by its ID, including related user and asset information.

---
+++ sanity/lib/suggested-topics/getSuggestedTopicById.ts
@@ -0,0 +1,30 @@
+import { client } from "../client";
+import { SuggestedTopic } from "@/sanity.types";
+
+export async function getSuggestedTopicById(
+  id: SuggestedTopic["_id"]
+): Promise<SuggestedTopic | null> {
+  return client.fetch(
+    `
+    *[_type == "suggestedTopic" && _id == $id][0] {
+      _id,
+      title,
+      "slug": slug,
+      description,
+      "iconUrl": icon.asset->url,
+      "bannerUrl": banner.asset->url,
+      status,
+      aiModerationResult,
+      "submittedBy": submittedBy-> {
+        _id,
+        username,
+        imageUrl
+      },
+      submittedAt,
+      _createdAt,
+      _updatedAt
+    }
+  `,
+    { id }
+  );
+}
