7acec92120110f25950b528306bc7737e1dd4253
ptakondrej <florixakcz@seznam.cz>
2025-11-21 12:24:41 +0100

    Refactor sidebar menu item rendering
    Moved sidebar menu item logic to a new SidebarItem component for improved modularity and maintainability. Menu icon references are now string-based and resolved via a mapping, simplifying the menu data structure.

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React from "react";
+import { SidebarMenuButton, SidebarMenuItem, useSidebar } from "./ui/sidebar";
+import Link from "next/link";
+import { Item } from "./app-sidebar";
+import {
+  Home,
+  Newspaper,
+  Search,
+  ShieldCheck,
+  TriangleAlert,
+  User,
+} from "lucide-react";
+
+type SidebarItemProps = {
+  item: Item;
+  isLoggedIn: boolean;
+  isAdmin: boolean;
+};
+
+const ICONS: Record<string, React.ComponentType> = {
+  Home,
+  Newspaper,
+  Search,
+  ShieldCheck,
+  TriangleAlert,
+  User,
+};
+
+const SidebarItem = ({ item, isLoggedIn, isAdmin }: SidebarItemProps) => {
+  const { toggleSidebar } = useSidebar();
+
+  const handleClick = () => {
+    toggleSidebar();
+  };
+
+  const { title, url, icon, adminOnly, loggedInOnly } = item;
+  if ((adminOnly && !isAdmin) || (loggedInOnly && !isLoggedIn)) {
+    return null;
+  }
+  const Icon = ICONS[icon];
+  return (
+    <SidebarMenuItem key={title}>
+      <SidebarMenuButton asChild>
+        <Link
+          href={url}
+          onClick={handleClick}
+          className="flex items-center gap-2"
+        >
+          <Icon />
+          <span>{title}</span>
+        </Link>
+      </SidebarMenuButton>
+    </SidebarMenuItem>
+  );
+};
+
+export default SidebarItem;
